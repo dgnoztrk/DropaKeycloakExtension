bc593a1fac2c80b7f7de3f0ecdb7960dde68430a
Doğan Öztürk <dogan.ozturk@mikro.com.tr>
2023-12-26 16:33:36 +0300

    Add project files.

---
+++ KeycloakExtension/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using KeycloakExtension.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace KeycloakExtension
+{
+	public static class ServiceCollectionExtensions
+	{
+        public static void AddKeycloak(this IServiceCollection services, KeycloakSettings keycloakSettings)
+		{
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+			{
+				options.Authority = $"{keycloakSettings.AuthorityUrl}/realms/{keycloakSettings.realmName}";
+				options.RequireHttpsMetadata = keycloakSettings.RequireHttpsMetadata;
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = keycloakSettings.ValidateIssuerSigningKey,
+					ValidateIssuer = keycloakSettings.ValidateIssuer,
+					ValidateAudience = keycloakSettings.ValidateAudience
+				};
+			});
+		}
+	}
+}
---
+++ KeycloakExtension/Models/KeycloakSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace KeycloakExtension.Models
+{
+	public class KeycloakSettings
+	{
+		public string AuthorityUrl { get; set; }
+		public string realmName { get; set; }
+        public bool RequireHttpsMetadata { get; set; }
+        public bool ValidateIssuerSigningKey { get; set; }
+        public bool ValidateIssuer { get; set; }
+        public bool ValidateAudience { get; set; }
+    }
+}
