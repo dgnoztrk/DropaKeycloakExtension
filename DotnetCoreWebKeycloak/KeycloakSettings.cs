b70c1a337292de204fc2d9ee0a6a5b06d0896649
dropa <info@doganozturk.net>
2023-12-31 01:05:50 +0300

    0

---
+++ DotnetCoreWebKeycloak/KeycloakSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace DotnetCoreWebKeycloak
+{
+    public class KeycloakSettings
+    {
+        public string Authority { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string CallbackPath { get; set; }
+        public string ResponseType { get; set; } = "code";
+        public bool SaveTokens { get; set; }
+        public bool RequireHttpsMetadata { get; set; }
+    }
+}
---
+++ DotnetCoreWebKeycloak/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotnetCoreWebKeycloak
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// For web UI projects
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="keycloakSettings"></param>
+        public static void AddKeycloak(this IServiceCollection services, KeycloakSettings keycloakSettings)
+        {
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie()
+                .AddOpenIdConnect(options =>
+                {
+                    options.Authority = keycloakSettings.Authority;
+                    options.ClientId = keycloakSettings.ClientId;
+                    options.ClientSecret = keycloakSettings.ClientSecret;
+                    options.CallbackPath = keycloakSettings.CallbackPath;
+                    options.ResponseType = keycloakSettings.ResponseType;
+                    options.SaveTokens = keycloakSettings.SaveTokens;
+                    options.RequireHttpsMetadata = keycloakSettings.RequireHttpsMetadata;
+                });
+        }
+    }
+}
